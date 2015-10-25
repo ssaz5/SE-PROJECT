fe34667a24d79b81b29b4c4f3f8f590dafdd9e6f
ssaz5 <ssaz_5@live.com>
2015-10-25 13:57:13 +0500

    Movement and shiz
    Tried somethings. Will Have to use BOX2D now.

--- test/core/src/com/mygdx/game/MyGdxGame.java
+++ test/core/src/com/mygdx/game/MyGdxGame.java
@@ -9,19 +9,44 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	int x;
+	int y;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		x = 100;
+		y=100;
+
+	}
+
+
+	public void update(){
+		x=x+1;
+		if((x%100 != 0)){
+			y++;
+
+		}
+		if(x%1600 == 0){
+			x = 0;
+
+		}
+		if(y%800 == 0){
+			y = 0;
+
+		}
+
+
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 0);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, x, y);
 		batch.end();
+		update();
 	}
 }
