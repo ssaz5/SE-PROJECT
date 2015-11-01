9c8c1798caef4b8e1bd588cf2346b76a6150fb0a
ssaz5 <ssaz_5@live.com>
2015-11-01 23:50:09 +0500

    nothing

--- test/core/src/com/badlogic/drop/Drop.java
+++ test/core/src/com/badlogic/drop/Drop.java
@@ -117,7 +117,7 @@ public class Drop extends ApplicationAdapter {
         Iterator<Rectangle> iter = raindrops.iterator();
         while(iter.hasNext()) {
             Rectangle raindrop = iter.next();
-            raindrop.y -= 200 * Gdx.graphics.getDeltaTime();
+            raindrop.y -= 500 * Gdx.graphics.getDeltaTime();
             if(raindrop.y + 64 < 0) iter.remove();
             if(raindrop.overlaps(bucket)) {
                 dropSound.play();
